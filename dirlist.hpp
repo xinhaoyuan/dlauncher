a72838ccc5ae4fb4e3cde8f07f07b8e2331632cf
Xinhao Yuan <xinhaoyuan@gmail.com>
2014-05-12 11:21:55 -0400

    clean up; version advanced

--- dirlist.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef __DLAUNCHER_DIRLIST_HPP__
-#define __DLAUNCHER_DIRLIST_HPP__
-
-#include <string>
-#include <vector>
-
-int dirlist(const std::string &dirname, std::vector<std::string> &r, const std::string &cache_file_prefix);
-
-#endif
