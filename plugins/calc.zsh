c6f04e1400c2a54bd48d2d1c0583fb8ff2e3b93a
xy2189 <xinhaoyuan@gmail.com>
2014-05-14 14:39:14 -0400

    fixing

--- plugins/calc.zsh
+++ plugins/calc.zsh
@@ -3,8 +3,8 @@
 while read line; do
     # process queries only
     if [[ ${line:0:1} == 'q' ]]; then
+        print "clear"
         if [[ ${line:1:1} == '?' ]]; then
-            print "clear"
             r=$(print "${line:2}" | bc -l 2>/dev/null)
             if [[ -n "$r" ]]; then
                 print "$r"
